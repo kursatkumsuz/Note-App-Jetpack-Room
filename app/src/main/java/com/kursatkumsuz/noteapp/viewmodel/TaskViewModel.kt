ed18437d4d5f2727c4ff442f827a4d369b736cde
DrHistory <kursatkumsuz@gmail.com>
2022-08-13 19:19:45 +0300

    add ViewModel

---
+++ app/src/main/java/com/kursatkumsuz/noteapp/viewmodel/TaskViewModel.kt
@@ -0,0 +1,35 @@
+package com.kursatkumsuz.noteapp.viewmodel
+
+import android.app.Application
+import android.widget.Toast
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.kursatkumsuz.noteapp.data.TaskDataBase
+import com.kursatkumsuz.noteapp.model.TaskModel
+import com.kursatkumsuz.noteapp.repo.TaskRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class TaskViewModel(application: Application) : AndroidViewModel(application) {
+    val taskList : LiveData<List<TaskModel>>
+    private val repository : TaskRepository
+
+    init {
+        val taskDao = TaskDataBase.invoke(application).taskDao()
+        repository = TaskRepository(taskDao)
+        taskList = repository.readTask
+    }
+
+    fun addTask(task : TaskModel) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.addTask(task)
+        }
+    }
+    fun deleteTask() {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.deleteTask()
+        }
+        Toast.makeText(getApplication() , "Deleted Tasks" , Toast.LENGTH_SHORT).show()
+    }
+}
